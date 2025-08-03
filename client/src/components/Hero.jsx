1d403695efde5019489a89f676959ac94a4695d7
anishdass <anishdassatoffice@gmail.com>
2025-08-03 18:14:36 +0100

    Working on dashboard

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate();
   return (
     <div className=' px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
       <div className=' text-center mb-6'>
@@ -15,7 +17,9 @@ const Hero = () => {
         </p>
 
         <div className=' flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
-          <button className=' bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>
+          <button
+            className=' bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'
+            onClick={() => navigate("/ai")}>
             Create
           </button>
           <button className=' bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>
