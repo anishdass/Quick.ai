4b974afbd561a05f40064e978a532ba2248c06be
anishdass <anishdassatoffice@gmail.com>
2025-08-27 16:50:24 +0100

    Dashboard Page completed

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -44,6 +44,7 @@ const Community = () => {
       );
 
       if (data.success) {
+        toast.success(data.message);
         fetchCreations();
       } else {
         toast.error(data.message);
@@ -55,30 +56,30 @@ const Community = () => {
 
   return (
     creations && (
-      <div className=' flex-1 h-full flex flex-col gap-4 p-6'>
+      <div className='flex-1 h-full flex flex-col gap-4 p-6'>
         Creations
-        <div className=' bg-white h-full w-full rounded-xl overflow-y-scroll'>
+        <div className='bg-white h-full w-full rounded-xl overflow-y-scroll'>
           {creations.map((creation, index) => (
             <div
               key={index}
-              className=' relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3'>
+              className='relative group inline-block pl-3 pt-3 w-full sm:w-1/2 lg:w-1/3'>
               <img
                 src={creation.content}
                 alt=''
-                className=' w-full h-full object-cover rounded-lg'
+                className='w-full h-full object-cover rounded-lg'
               />
-              <div className=' absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg'>
-                <p className=' text-sm hidden group-hover:block'>
+              <div className='absolute inset-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg'>
+                <p className='text-sm hidden group-hover:block'>
                   {creation.prompt}
                 </p>
-                <div className=' flex gap-1 items-center'>
+                <div className='flex gap-1 items-center'>
                   <p>{creation.likes.length}</p>
                   <Heart
                     onClick={() => toggleLikeClick(creation.id)}
-                    className={` min-w-5 h-5 hover:scale-110 cursor-pointer ${
+                    className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
                       creation.likes.includes(user.id)
-                        ? " fill-red-500 text-red-600"
-                        : " text-white"
+                        ? "fill-red-500 text-red-600"
+                        : "text-white"
                     }`}
                   />
                 </div>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,19 +3,35 @@ import { dummyCreationData } from "../assets/assets";
 import { Gem, Sparkles } from "lucide-react";
 import { Protect } from "@clerk/clerk-react";
 import CreationItem from "../components/CreationItem";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+  const { loading, setLoading, axios, token } = useAppContext();
   const [creations, setCreations] = useState([]);
 
   const getDashboardData = async () => {
-    setCreations(dummyCreationData);
+    try {
+      setLoading(true);
+      const { data } = await axios.get("/api/user/get-user-creations", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (data.success) {
+        setCreations(data.content);
+      } else {
+        toast.error(data.message);
+      }
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
     getDashboardData();
   }, []);
 
-  return (
+  return !loading ? (
     <div className=' h-full overflow-y-scroll p-6'>
       <div className=' flex justify-start gap-4 flex-wrap'>
         {/* Total creations card */}
@@ -44,12 +60,23 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className=' space-y-3'>
-        <p className=' mt-6 mb-4'>Recent Creations</p>
-        {creations.map((item) => (
-          <CreationItem key={item.id} item={item} />
-        ))}
-      </div>
+
+      {loading ? (
+        <div className=' flex justify-center items-center h-3/4'>
+          <div className=' animate-spin rounded-full h-11 w-11 border-3 border-purple-500 border-t-transparent'></div>
+        </div>
+      ) : (
+        <div className=' space-y-3'>
+          <p className=' mt-6 mb-4'>Recent Creations</p>
+          {creations.map((item) => (
+            <CreationItem key={item.id} item={item} />
+          ))}
+        </div>
+      )}
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+      <div className='w-12 h-12 my-1 rounded-full border-4 border-t-transparent animate-spin'></div>
     </div>
   );
 };
